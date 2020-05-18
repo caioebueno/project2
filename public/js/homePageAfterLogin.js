f8251701ef344c32229c15eef0c2f99e8338f70e
Noah Aflalo <noahaflalo3@gmail.com>
2020-05-17 20:33:55 -0400

    version 1.0.8

--- public/js/homePageAfterLogin.js
+++ public/js/homePageAfterLogin.js
@@ -1,31 +1,30 @@
-
 $.ajax({
-    url: "/api/services",
-    method: "GET"
-  }).then(result => {
-    var container = document.getElementById("containerService");
-    container.innerHTML = "";
-    result.forEach(obj => {
-      console.log(obj);
-      var createDiv = document.createElement("div");
-      createDiv.setAttribute("class", "card");
-      createDiv.setAttribute("style", "width: 18rem;");
-      var html =
-        `<div class="serviceItem col-md-12">
+  url: "/api/services",
+  method: "GET"
+}).then(result => {
+  var container = document.getElementById("containerService");
+  container.innerHTML = "";
+  result.forEach(obj => {
+    console.log(obj);
+    var createDiv = document.createElement("div");
+    createDiv.setAttribute("class", "card");
+    createDiv.setAttribute("style", "width: 18rem;");
+    var html =
+      `<div class="serviceItem col-md-12">
               <div class="card" href="">
                   <div class="card-img-top card-img-top-250">
                       <img class="img-fluid" src="` +
-                      obj.img +
-        `" alt="Carousel 1">
+      obj.img +
+      `" alt="Carousel 1">
                   </div>
                   <div class="card-block p-t-2">
                       <h3 class=" text-wide p-b-2">` +
-                      obj.title +
-        `</h3>
+      obj.title +
+      `</h3>
                       <h6 class="small">
                           <a>` +
-                          obj.desc +
-        `</a>
+      obj.desc +
+      `</a>
                           <hr>
                           <div>
                           <a>(350) Reviews ⭐️⭐️⭐️⭐️</a>
@@ -35,11 +34,10 @@ $.ajax({
                   </div>
               </div>
           </div>`;
-      createDiv.innerHTML = html;
-      container.append(createDiv);
-    });
+    createDiv.innerHTML = html;
+    container.append(createDiv);
   });
-
+});
 
 function searchService() {
   var searchStr = document.getElementById("searchInput").value;
@@ -50,7 +48,7 @@ function searchService() {
   }).then(result => {
     var container = document.getElementById("containerService");
     container.innerHTML = "";
- 
+
     result.forEach(obj => {
       console.log(obj);
       var createDiv = document.createElement("div");
@@ -61,16 +59,16 @@ function searchService() {
               <div class="card" href="">
                   <div class="card-img-top card-img-top-250">
                       <img class="img-fluid" src="` +
-                      obj.img +
+        obj.img +
         `" alt="Carousel 1">
                   </div>
                   <div class="card-block p-t-2">
                       <h3 class=" text-wide p-b-2">` +
-                      obj.title +
+        obj.title +
         `</h3>
                       <h6 class="small">
                           <a>` +
-                          obj.desc +
+        obj.desc +
         `</a>
                           <hr>
                           <div>
